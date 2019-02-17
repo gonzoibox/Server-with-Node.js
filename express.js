2f8456ca2714fc5acb1c881a5a4969ff76aa58bb
anton_davidovich <dvd14@ukr.net>
2019-02-17 15:15:18 +0200

    fix pug file - form submission

--- express.js
+++ express.js
@@ -8,37 +8,39 @@ server.set('view engine', 'pug');
 server.use(express.urlencoded({ extended: true }));
 server.use(cookieParser());
 server.use(express.static('public'));
+
 server.get('/', (req, res) => {
     const username = req.cookies.username;
+    console.log(req.body.username);
     res.render('index', {
         username
     });
 });
 
 server.post('/', (req, res) => {
-    res.cookie('username', username);
+    res.cookie('username', req.body.username);
     res.redirect('/');
 });
 
-server.get('/suggestions', (res, req) => {
-    //Show sugestion list
-    throw new Error('Not implemented');
-});
+    server.get('/suggestions', (res, req) => {
+        //Show sugestion list
+        throw new Error('Not implemented');
+    });
 
-server.post('/suggestions', (res, req) => {
-    //Create suggestion
-    //Redirect list
-    throw new Error('Not implemented');
-});
+    server.post('/suggestions', (res, req) => {
+        //Create suggestion
+        //Redirect list
+        throw new Error('Not implemented');
+    });
 
-server.get('/suggestions/1', (res, req) => {
-    //Show suggestion
-    throw new Error('Not implemented');
-});
+    server.get('/suggestions/1', (res, req) => {
+        //Show suggestion
+        throw new Error('Not implemented');
+    });
 
-server.post('/suggestions/1', (res, req) => {
-    //Create suggestion
-    throw new Error('Not implemented');
-});
+    server.post('/suggestions/1', (res, req) => {
+        //Create suggestion
+        throw new Error('Not implemented');
+    });
 
 server.listen(3000, 'localhost', () => console.log("Server is up///"));
